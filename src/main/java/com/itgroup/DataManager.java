38a6d0792a8ba50196f42f2ffd86c5fd15bccd32
bluesky <bluesky@naver.com>
2025-08-20 16:53:40 +0900

    두번째 깃허브

--- src/main/java/com/itgroup/DataManager.java
+++ src/main/java/com/itgroup/DataManager.java
@@ -1,5 +1,6 @@
 package com.itgroup;
 
+import com.itgroup.bean.Board;
 import com.itgroup.bean.Member;
 import com.itgroup.dao.MemberDao;
 
@@ -7,14 +8,50 @@ import java.util.List;
 import java.util.Scanner;
 
 // 메인 클래스 대신 실제 모든 업무를 총 책임지는 매니저 클래스
-public class MemberManager {
-    private MemberDao dao = null ; // 실제 데이터 베이스와 연동하는 다오 클래스
+public class DataManager {
+    private MemberDao mdao = null ; // 실제 데이터 베이스와 연동하는 다오 클래스
+    private BoardDao bdao = null ;
     private Scanner scan = null; //회원정보 입력 받기 위한 스캐너 장치
 
-    public MemberManager() {
-        this.dao = new MemberDao() ;
+    public DataManager() {
+        this.mdao = new MemberDao() ;
+        this.bdao = new BoardDao();
         this.scan = new Scanner(System.in);
     }
+
+
+    public void updateData() {
+        int cnt= -1;
+
+        System.out.println("수정하고자 하는 회원 id 입력 : ");
+        String findId = scan.next(); // yusin 입력
+
+        // 여기서 bean은 이전에 입력했던 나의 정보입니다.
+        Member bean = mdao.getMemberOne(findId);
+
+        // 편의상 내 이름과 결혼 여부를 변경해 보겠습니다.
+        System.out.println("이름 입력 : ");
+        String name = scan.next();
+
+        System.out.println("결혼 여부 입력 : ");
+        String marriage = scan.next();
+
+        bean.setName(name);
+        bean.setMarriage(marriage);
+
+        cnt = mdao.updateData();
+
+        if(cnt == -1){
+            System.out.println("업데이트 실패");
+
+        }else if(cnt ==1){
+            System.out.println("업데이트 성공");
+
+        }else{
+
+        }
+
+
 
     public void insertData() {
         Member bean = new Member();
@@ -38,14 +75,22 @@ public class MemberManager {
         bean.setAddress("서대문");
         bean.setManager(null);
 
+        cnt = mdao.insertData(bean);
 
-        cnt = dao.insertData(bean);
+        if(cnt ==-1){
+            System.out.println("회원가입 실패");
+        }else if(cnt == 1){
+            System.out.println("회원 아이디 " + id + "로 가입 성공");
+        }else{
+
+        }
+
     }
 
 
     public void getMemberOne() {
         String findId = "xx" ; // 찾고자 하는 회원
-        Member someone = dao.getMemberOne(findId);
+        Member someone = mdao.getMemberOne(findId);
 
         if(someone == null){
             System.out.println("찾으시는 회원이 존재하지 않습니다.");
@@ -61,7 +106,7 @@ public class MemberManager {
     public void deleteData() { // 나의 id를 사용한 탈퇴
         String id = "yusin";
         int cnt = - 1 ;
-        cnt = dao.deleteData(id);
+        cnt = mdao.deleteData(id);
 
         if(cnt == -1){
             System.out.println("회원 탈퇴 실패(접속, 네트워크 오류)");
@@ -75,7 +120,7 @@ public class MemberManager {
     }
 
     public void selectAll() { // 모든 회원 정보 조회
-        List<Member> members = dao.selectAll();
+        List<Member> members = mdao.selectAll();
         System.out.println("이름\t급여\t주소");
         for(Member bean:members){
             String name = bean.getName();
@@ -88,7 +133,7 @@ public class MemberManager {
 
     public void findByGender() {
         String tempgender = "여자" ;
-        List<Member> mydata = dao.findByGender(tempgender);
+        List<Member> mydata = mdao.findByGender(tempgender);
         System.out.println("이름\t급여\t주소\t성별");
         for(Member bean:mydata){
             String name = bean.getName();
@@ -101,7 +146,7 @@ public class MemberManager {
     }
 
     public void getSize() { // 몇 명의 회원인지 조회하는 구문입니다.
-        int cnt = dao.getSize();
+        int cnt = mdao.getSize();
         String message ;
         if(cnt == 0){
             message = "검색된 회원이 존재하지 않습니다.";
@@ -113,4 +158,35 @@ public class MemberManager {
 
 
 
+    }
+
+
+    public void selectAllBoard() {
+        List<Board> boardList = bdao.selectAll();
+
+        System.out.println("글번호\t작성자\t글제목\t글내용");
+        for(Board bean : boardList){
+            int no = bean.getNo();
+            String writer = bean.getWriter();
+            String subject = bean.getSubject();
+            String content = bean.getContent();
+            String messsage = no + "\t" + writer + "\t" + subject + "\t" + content;
+            System.out.println(messsage);
+        }
+    }
+
+    public void selectEvenData() {
+        // 게시물 번호가 짝수인 항목들만 조회해 봅니다.
+        List<Board> boardList = bdao.selectEvenData();
+
+        System.out.println("글번호\t작성자\t글제목\t글내용");
+        for(Board bean : boardList){
+            int no = bean.getNo();
+            String writer = bean.getWriter();
+            String subject = bean.getSubject();
+            String content = bean.getContent();
+            String messsage = no + "\t" + writer + "\t" + subject + "\t" + content;
+            System.out.println(messsage);
+        }
+    }
 }
--- src/main/java/com/itgroup/dao/MemberDao.java
+++ src/main/java/com/itgroup/dao/MemberDao.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 // 데이터 베이스와 직접 연동하여 CRUD 작업을 수행해주는 DAO 클래스
-public class MemberDao {
+public class MemberDao extends SuperDao{
     public MemberDao() {
-        // 드라이버 관련 OracleDriver 클래스는 ojdbc6.jar 파일에 포함되어 있는 자바 클래스입니다.
-        String driver = "oracle.jdbc.driver.OracleDriver";
 
-        try {
-            Class.forName(driver); // 동적 객체 생성하는 문법입니다.
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("해당 드라이버가 존재하지 않습니다.");
-            e.printStackTrace();
         }
+    }
+
+    public int updateData(Member bean) {
+    //수정된 나의 정보 bean을 사용하여 데이터 베이스에 수정합니다.
+        int cnt= -1;
+
+        String sql = "update members set name = ?, password = ?, gender = ?, birth = ?, marriage = ?, salary = ?, address = ?, manager = ?";
+        sql += " where id = ? ";
+
+        return cnt;
     }
 
 
@@ -26,26 +28,26 @@ public class MemberDao {
         // 웹 페이지에서 회원 정보를 입력하고 '가입' 버튼을 눌렀습니다.
         int cnt = -1;
 
-        String sql = "insert into members(id, name, password, gender, birth, marriage, salary, address, manager)";
-        sql += " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "update members set name = ?, password = ?, gender = ?, birth = ?, marriage = ?, salary = ?, address = ?, manager = ?";
+        sql += " where id = ? ";
 
         Connection conn = null;
         PreparedStatement pstmt = null;
 
         try{
-            conn = this.getConnection();
+            conn = super.getConnection();
             pstmt = conn.prepareStatement(sql);
 
-            //id, name, password, gender, birth, marriage, salary, address, manager
-            pstmt.setString(1, bean.getId());
+
             pstmt.setString(1, bean.getName());
-            pstmt.setString(1, bean.getPassword());
-            pstmt.setString(1, bean.getGender());
-            pstmt.setString(1, bean.getBirth());
-            pstmt.setString(1, bean.getMarriage());
-            pstmt.setInt(1, bean.getSalary());
-            pstmt.setString(1, bean.getAddress());
-            pstmt.setString(1, bean.getManager());
+            pstmt.setString(2, bean.getPassword());
+            pstmt.setString(3, bean.getGender());
+            pstmt.setString(4, bean.getBirth());
+            pstmt.setString(5, bean.getMarriage());
+            pstmt.setInt(6, bean.getSalary());
+            pstmt.setString(7, bean.getAddress());
+            pstmt.setString(8, bean.getManager());
+            pstmt.setString(9, bean.getId());
 
             cnt = pstmt.executeUpdate();
             conn.commit();
@@ -68,23 +70,6 @@ public class MemberDao {
         return cnt;
     }
 
-    public Connection getConnection() {
-        Connection conn = null; // 접속 객체
-
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        String id = "oraman";
-        String password = "oracle";
-
-        try {
-            conn = DriverManager.getConnection(url, id, password);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return conn;
-    }
-
     public int getSize() {
         String sql = "select count(*) as cnt from members  ";
         PreparedStatement pstmt = null;
@@ -92,7 +77,7 @@ public class MemberDao {
         Connection conn = null;
         int cnt = 0; // 검색된 회원 명수
         try {
-            conn = this.getConnection(); // 접속 객체 구하기
+            conn = super.getConnection(); // 접속 객체 구하기
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
 
@@ -133,7 +118,7 @@ public class MemberDao {
         String sql = "select * from members where id = ? ";
 
         try {
-            conn = this.getConnection();
+            conn = super.getConnection();
             pstmt = conn.prepareStatement(sql);
 
             pstmt.setString(1, id);
@@ -145,7 +130,7 @@ public class MemberDao {
                 bean.setName(rs.getString("name"));
                 bean.setPassword(rs.getString("password"));
                 bean.setGender(rs.getString("gender"));
-                bean.setBirth(rs.getString("birth"));
+                bean.setBirth(String.valueOf(rs.getDate("birth")));
                 bean.setMarriage(rs.getString("marriage"));
                 bean.setSalary(rs.getInt("salary"));
                 bean.setAddress(rs.getString("address"));
@@ -180,7 +165,7 @@ public class MemberDao {
         Connection conn = null;
 
         try{
-            conn = this.getConnection();
+            conn = super.getConnection();
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, id);
 
@@ -219,7 +204,7 @@ public class MemberDao {
         String sql = "select * from members order by name asc ";
 
         try {
-            conn = this.getConnection();
+            conn = super.getConnection();
             pstmt = conn.prepareStatement(sql);
             rs = pstmt.executeQuery();
 
@@ -275,7 +260,7 @@ public class MemberDao {
         List<Member> members = new ArrayList<Member>();
 
         try {
-            conn = this.getConnection();
+            conn = super.getConnection();
             pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, gender);
             rs = pstmt.executeQuery();
@@ -315,6 +300,7 @@ public class MemberDao {
 
         return members;
     }
+
 
 
 }
